70d7029424dce8680d2ff6c8a7538923924d63c2
Andrew Best <andrew.best@readify.net>
2015-06-09 08:56:40 +1000

    Added focus behavior example.

---
+++ ModernWPF.Client/Features/Behaviors/FocusBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace ModernWPF.Client.Features.Behaviors
+{
+    public class FocusBehavior : Behavior<FrameworkElement>
+    {
+        protected override void OnAttached()
+        {
+            AssociatedObject.Loaded += Focus;
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.Loaded -= Focus;
+        }
+
+        private void Focus(object sender, RoutedEventArgs routedEventArgs)
+        {
+            AssociatedObject.Focus();
+
+            var textbox = AssociatedObject as TextBox;
+            if (textbox != null)
+            {
+                textbox.CaretIndex = textbox.Text.Length;
+            }
+        }
+    }
+}
